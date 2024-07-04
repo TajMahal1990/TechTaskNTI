802dae73df2a6a82cd4dc6602bc3764f113cfb93
iaroslavneudachin <n89500554589@gmail.com>
2024-07-04 17:32:30 +0600

    Initial

--- server/src/main/java/com/example/server/ServerActivity.kt
+++ server/src/main/java/com/example/server/ServerActivity.kt
@@ -1,23 +1,21 @@
 package com.example.server
 
-package com.example.swipeproject.server
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ServerActivity : ComponentActivity() {
-    private val viewModel: ServerViewModel by viewModel
+ //   private val viewModel: ServerViewModel by viewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SwipeProjectTheme {
+      //      SwipeProjectTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    ServerScreen(viewModel)
+         //           ServerScreen(viewModel)
                 }
             }
         }
